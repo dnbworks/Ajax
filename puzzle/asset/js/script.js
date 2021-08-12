4cd59c4b57843e8b3394d2fbd39d15d8ebacac1a
dnbworks <dooyongnsaakobalderama@gmail.com>
2021-08-13 00:12:38 +0800

    add puzzle folder

---
+++ puzzle/asset/js/script.js
@@ -0,0 +1,103 @@
+
+window.onload = initpage;
+
+
+
+function initpage(){
+    var cells = document.querySelectorAll("td");
+
+    cells.forEach((cell) => {
+        addEventHandler(cell, 'click', tileClick);
+    });
+
+
+}
+
+function tileClick(e){
+    if(cellIsEmpty(this)){
+        alert("please click on a numbered title");
+        return;
+    }
+
+    var currentRow = this.id.charAt(4);
+    var currentColumn = this.id.charAt(5);
+
+    // console.log(currentRow, currentColumn);
+
+    // checking above
+    if(currentRow > 1){
+        var testRow = parseInt(currentRow) - 1;
+        var testCellId = "cell" + testRow + currentColumn;
+        var testCell = document.getElementById(testCellId);
+        if(cellIsEmpty(testCell)){
+            // console.log(testCell);
+            swapTiles(this, testCell);
+            return;
+        }
+        
+    }
+
+    // checking below
+    if(currentRow < 4){
+        var testRow = parseInt(currentRow) + 1;
+        var testCellId = "cell" + testRow + currentColumn;
+        var testCell = document.getElementById(testCellId);
+
+        if(cellIsEmpty(testCell)){
+            // console.log(testCell);
+            swapTiles(this, testCell);
+            return;
+        }
+        
+    }
+
+     // checking left
+     if(currentColumn > 1){
+        var testCol = parseInt(currentColumn) - 1;
+        var testCellId = "cell" + currentRow + testCol;
+        var testCell = document.getElementById(testCellId);
+
+        if(cellIsEmpty(testCell)){
+            // console.log(testCell);
+            swapTiles(this, testCell);
+            return;
+        }
+        
+    }
+
+      // checking right
+    if(currentColumn < 4){
+        var testCol = parseInt(currentColumn) + 1;
+        var testCellId = "cell" + currentRow + testCol;
+        var testCell = document.getElementById(testCellId);
+
+        if(cellIsEmpty(testCell)){
+            // console.log(testCell);
+            swapTiles(this, testCell);
+            return;
+        }
+        
+    }
+
+    // alert("Please click a tile next to an emty cell");
+}
+
+function cellIsEmpty(cell){
+    var image = cell.firstElementChild;
+   
+    if(image.alt == "empty")
+        return true;
+     else 
+        return false;
+
+}
+
+function swapTiles(targetTile, destinationTile){
+    selectedImage = targetTile.firstElementChild;
+    destinatedImage = destinationTile.firstElementChild;
+
+    targetTile.appendChild(destinatedImage);
+    destinationTile.appendChild(selectedImage);
+    // console.log(selectedImage, destinatedImage);
+
+}
---
+++ puzzle/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puzzle</title>
+
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header></header>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4 col-lg-4">
+                    <h1>Webville Puzzles</h1>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6" id="puzzle-grid">
+                    <table cellspacing="0" cellpadding="0">
+                        <tr>
+                            <td id="cell11">
+                                <img src="asset/img/7.jpg" alt="7">
+                            </td>
+                            <td id="cell12">
+                                <img src="asset/img/6.jpg" alt="6">
+                            </td>
+                            <td id="cell13">
+                                <img src="asset/img/14.jpg" alt="14">
+                            </td>
+                            <td id="cell14">
+                                <img src="asset/img/11.jpg" alt="11">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="cell21">
+                                <img src="asset/img/12.jpg" alt="12">
+                            </td>
+                            <td id="cell22">
+                                <img src="asset/img/empty.jpg" alt="empty">
+                            </td>
+                            <td id="cell23">
+                                <img src="asset/img/3.jpg" alt="5">
+                            </td>
+                            <td id="cell24">
+                                <img src="asset/img/13.jpg" alt="13">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="cell31">
+                                <img src="asset/img/8.jpg" alt="8">
+                            </td>
+                            <td id="cell32">
+                                <img src="asset/img/10.jpg" alt="10">
+                            </td>
+                            <td id="cell33">
+                                <img src="asset/img/1.jpg" alt="1">
+                            </td>
+                            <td id="cell34">
+                                <img src="asset/img/15.jpg" alt="15">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="cell41">
+                                <img src="asset/img/4.jpg" alt="4">
+                            </td>
+                            <td id="cell42">
+                                <img src="asset/img/2.jpg" alt="2">
+                            </td>
+                            <td id="cell43">
+                                <img src="asset/img/9.jpg" alt="9">
+                            </td>
+                            <td id="cell44">
+                                <img src="asset/img/5.jpg" alt="5">
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <footer></footer>
+    </div>
+    <script src="asset/js/utils.js"></script>
+    <script src="asset/js/script.js"></script>
+</body>
+</html>
